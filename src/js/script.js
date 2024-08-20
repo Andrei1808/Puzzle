a4be4920a9e82e924f2a770ecc4a3348f6fe36dd
Andrei <kaliarovich93@gmail.com>
2024-08-21 00:22:43 +0300

    feat: add script.js

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+import { getMatrix } from "./helpers/getMatrix.js";
+import { setPositionItems } from "./helpers/setPositionItems.js";
+import { shuffleArray } from "./helpers/shuffleArray.js";
+import { buttonsEvents } from "./events/buttonsEvents..js";
+import { clickEvents } from "./events/clickEvents.js";
+import { shuffleEvent } from "./events/shuffleEvent.js";
+
+
+
+export const containerNode = document.getElementById("fifteen");
+export const itemNodes = Array.from(containerNode.querySelectorAll(".item"));
+export const countItems = 16;
+
+if (itemNodes.length !== 16) {
+  throw new Error(`Must be ${countItems} items in HTML`);
+}
+
+/** Position */
+itemNodes[countItems - 1].style.display = "none";
+let matrix = getMatrix(itemNodes.map((item) => Number(item.dataset.matrixId)));
+
+setPositionItems(matrix);
+
+/** --------------------------------------------EVENTS----------------------------------- */
+
+/** Shuffle */
+document.getElementById("shuffle").addEventListener("click", () => shuffleEvent(matrix));
+
+/** Change position by click*/
+export const blankNumber = 16;
+containerNode.addEventListener("click", (event) => clickEvents(event, matrix));
+
+/** Change position by keydown */
+window.addEventListener("keydown", (event)=>buttonsEvents(event, matrix));
+
+
+
+
