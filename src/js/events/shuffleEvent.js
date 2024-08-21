b972a5624611a4889f44245d8d49e092751e64d1
Andrei <kaliarovich93@gmail.com>
2024-08-21 21:12:38 +0300

    fix: remove shuffleEvent module

--- src/js/events/shuffleEvent.js
+++
@@ -1,9 +0,0 @@
-import { shuffleArray } from "../helpers/shuffleArray.js";
-import { getMatrix } from "../helpers/getMatrix.js";
-import { setPositionItems } from "../helpers/setPositionItems.js";
-
-export function shuffleEvent(matrix){
-  const shuffledArray = shuffleArray(matrix.flat());
-  matrix = getMatrix(shuffledArray);
-  setPositionItems(matrix);
-} 
